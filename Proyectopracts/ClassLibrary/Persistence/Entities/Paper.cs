6160bcdd11837eecd5299346717d0f13ace0a5d6
Gema Vidagany Viel <GVIDVIE@upv.edu.es>
2022-10-14 10:16:26 +0200

    creacion clases parciales

--- Proyectopracts/ClassLibrary/Persistence/Entities/Paper.cs
+++ Proyectopracts/ClassLibrary/Persistence/Entities/Paper.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassLibrary.Persistence.Entities
+namespace Magazine.Entities
 {
-    internal class Class1
+    public partial class Paper
     {
     }
 }
