22bce0c52190e8b9caf947a06ceb8ceac1a17cb2
Javier Sánchez Martínez <JASANMA5@upv.edu.es>
2022-10-14 10:49:17 +0200

    Person - Both

--- Proyectopracts/ClassLibrary/Persistence/Entities/Person.cs
+++ Proyectopracts/ClassLibrary/Persistence/Entities/Person.cs
@@ -7,8 +7,20 @@ using System.Threading.Tasks;
 namespace Magazine.Entities
 {
     public partial class Person 
-    { 
-    
+    {
+        public Person()
+
+        {
+            CoAuthorPapers = new List<Paper>();
+        }
+        public Person(string Id, string Name, string Surname)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Surname = Surname;
+            this.CoAuthorPapers = new List<Paper>();
+        }
+
     }   
 
     
