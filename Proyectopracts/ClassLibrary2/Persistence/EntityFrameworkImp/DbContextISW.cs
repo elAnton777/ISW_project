5a455efd751b48674319dfc27401a7f25c94e2e2
Gema Vidagany Viel <GVIDVIE@upv.edu.es>
2022-10-26 19:10:09 +0200

    using...

--- Proyectopracts/ClassLibrary2/Persistence/EntityFrameworkImp/DbContextISW.cs
+++ Proyectopracts/ClassLibrary2/Persistence/EntityFrameworkImp/DbContextISW.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data.Entity;
 using System.Reflection;
 using System.Data.Entity.Validation;
+using Magazine.Entities;
 
 namespace Magazine.Persistence
 {
--- Proyectopracts/ClassLibrary2/Persistence/EntityFrameworkImp/EntityFrameworkDAL.cs
+++ Proyectopracts/ClassLibrary2/Persistence/EntityFrameworkImp/EntityFrameworkDAL.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
+using Magazine.Entities;
 
 namespace Magazine.Persistence
 {
