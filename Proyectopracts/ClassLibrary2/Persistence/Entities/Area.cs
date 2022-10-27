358ef18bdfb98dc8a75e5c6d2a13e1d0d83a9ac8
elAnton777 <alejandro.antoncabezas@gmail.com>
2022-10-27 19:09:59 +0200

    Capa Persistencia Finalizada

--- Proyectopracts/ClassLibrary2/Persistence/Entities/Area.cs
+++ Proyectopracts/ClassLibrary2/Persistence/Entities/Area.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,19 +21,20 @@ namespace Magazine.Entities
             get;
             set;
         }
-
+        [InverseProperty("BelongingArea")]
         public virtual ICollection<Paper> Papers
         {
             get;
             set;
         }
 
+        [InverseProperty("EvaluationPendingArea")]
         public virtual ICollection<Paper> EvaluationPending 
         {
             get;
             set;
         }
-        
+        [InverseProperty("PublicationPendingArea")]
         public virtual ICollection<Paper> PublicationPending 
         {
             get; 
