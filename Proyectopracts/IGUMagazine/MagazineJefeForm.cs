875126ff9a33186430c17356d37cefa28554a7d9
Javier Sánchez Martínez <JASANMA5@upv.edu.es>
2022-12-15 14:11:40 +0100

    Diseño MagazineJefeForm

--- Proyectopracts/IGUMagazine/MagazineJefeForm.cs
+++ Proyectopracts/IGUMagazine/MagazineJefeForm.cs
@@ -31,5 +31,25 @@ namespace IGUMagazine
         {
 
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
