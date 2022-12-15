c5ba8e7da804dd9b1e99eb697860260be1a0e08e
Eduard Ramirez Montagud <ramirezmontagudeduard@gmail.com>
2022-12-15 20:05:29 +0100

    Añadido flow a los forms

--- Proyectopracts/IGUMagazine/SelectActionForm.cs
+++ Proyectopracts/IGUMagazine/SelectActionForm.cs
@@ -10,11 +10,39 @@ using System.Windows.Forms;
 
 namespace IGUMagazine
 {
-    public partial class Form1 : Form
+    public partial class SelectActionForm : Form
     {
-        public Form1()
+        public SelectActionForm()
         {
             InitializeComponent();
+        }
+
+        private void EnviarArticulo_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            SendArticleForm sendArticleForm= new SendArticleForm();
+            sendArticleForm.ShowDialog();
+        }
+
+        private void EvaluarArticulo_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            EvaluarArticuloForm evaluarArticuloForm = new EvaluarArticuloForm();
+            evaluarArticuloForm.ShowDialog();
+        }
+
+        private void ListadoDeArticulos_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ListaArticulos listaArticulos = new ListaArticulos();
+            listaArticulos.ShowDialog();
+        }
+
+        private void ConfeccionarEjemplar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ConfeccionarEjemplar confeccionarEjemplar = new ConfeccionarEjemplar();
+            confeccionarEjemplar.ShowDialog();
         }
     }
 }
